7ea6a984b90ae940e159de2b70ec4cd76d323ab3
Awowole Hammad <lexycon002@gmail.com>
2023-12-19 09:21:16 +0100

    hutis web portfolio

---
+++ script.js
@@ -0,0 +1,24 @@
+// Script implemented by woman dev ---- samuella aglago
+// The script begins here
+const menu = document.getElementsByClassName("menu-itm");
+const icon = document.getElementById("menu_icon");
+const menu_list = document.getElementById("menu_list");
+
+
+//adding a click event listener to icon
+icon.addEventListener('click', ()=> {
+    if (icon.classList.contains("exit"))
+    {
+        icon.classList.remove("exit");
+        icon.classList.add("reverse_animation");
+        menu_list.classList.add("noshow");
+    }
+    else
+    {
+        icon.classList.add("exit");
+        icon.classList.remove("reverse_animation");
+        menu_list.classList.remove("noshow");
+        menu_list.classList.add("show");
+    }
+});
+// The script ends here
